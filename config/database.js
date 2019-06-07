b3283a476599de56ff43a0501d6839dbd38f4156
rreddypwc <ramamohan.reddy@us.pwc.com>
2019-06-07 09:52:39 -0400

    update connectionstring access

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
-    localUrl: 'mongodb://localhost/meanstacktutorials'
+    //remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    remoteUrl : 'mongodb://mean-demo:76nOCTbNVzuxxqKZ6YmaZr2WJaH2UwnduktTDtbHQ62XvuwHuCheCXxeb2pGM51Ikga0SbnTdA00uvgL8R0fzQ==@mean-demo.documents.azure.com:10255/?ssl=true&replicaSet=globaldb';
+    localUrl: 'mongodb://localhost/mean'
 };
